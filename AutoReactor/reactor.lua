88164b331d567887f7eb3862a4fa35233e6c44a5
finlaywashere <finman292004@protonmail.com>
2021-04-24 19:11:08 -0400

    Replaced tabs with spaces

--- AutoReactor/reactor.lua
+++ AutoReactor/reactor.lua
@@ -5,14 +5,14 @@ local MAX_RF = 10000000
 
 while(true)
 do
-	local energy = reactor.getEnergyStored()
-	local percentage = energy / MAX_RF
-	if percentage <= .1 then
-		reactor.setActive(true)
-		print("Activating Reactor")
-	elseif percentage >= .9 then
-		reactor.setActive(false)
-		print("Deactivating Reactor")
-	end
-	os.sleep(30)
+  local energy = reactor.getEnergyStored()
+  local percentage = energy / MAX_RF
+  if percentage <= .1 then
+    reactor.setActive(true)
+    print("Activating Reactor")
+  elseif percentage >= .9 then
+    reactor.setActive(false)
+    print("Deactivating Reactor")
+  end
+  os.sleep(30)
 end
